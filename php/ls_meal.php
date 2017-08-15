22f184ebea9f677903b02f1e4c991e6709b3228c
Andrei Ivanov <andrei.i.ivanov@commandus.com>
2017-08-15 14:48:12 +0900

    1

---
+++ php/ls_meal.php
@@ -0,0 +1,11 @@
+<?php
+require "buynshare.php";
+
+// List of meal
+$locale = $_GET['locale'];
+$meals = ls_meal($locale);
+
+header('Content-Type: application/octet-stream');
+echo fb_meals($meals);
+
+?>
